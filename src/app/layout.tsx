7b7ea8de9969cf6c90fad43b5ef93e0cc2548072
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-08-22 21:37:31 +0700

    add theme provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { ThemeProvider } from "./theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`${inter.className} bg-slate-50 dark:bg-[#0d1117] duration-700`}
+      >
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <main>{children}</main>
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
