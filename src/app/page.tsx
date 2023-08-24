3150ed87f9cb0e88e3d1e126ef6480fa9b5328fb
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-08-24 21:44:45 +0700

    add menu component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,15 @@
 import Image from "next/image";
 import { useState, useEffect } from "react";
 import { useTheme } from "next-themes";
-import Header from "./components/header/header.component";
+import Header from "./components/header.component";
+import Menu from "./components/menu.component";
 
 export default function Home() {
   const { theme, setTheme } = useTheme();
   return (
     <main className={`px-5 pt-3 sm:px-9 sm:pt-5`}>
       <Header />
+      <Menu />
     </main>
   );
 }
