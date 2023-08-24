cda86d90d1735a8766415c6ce4c15916813a4782
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-08-24 21:45:15 +0700

    create menu component

---
+++ src/app/components/menu.component.tsx
@@ -0,0 +1,69 @@
+import Image from "next/image";
+import React from "react";
+
+const Menu = () => {
+  return (
+    <main>
+      <div className={` flex justify-between items-center`}>
+        <p className={`font-bold`}>Baca Al-Qur'an</p>
+        <button
+          className={`bg-green-500 px-2 py-1 text-sm font-semibold rounded`}
+        >
+          Bookmark
+        </button>
+      </div>
+      <div className={`mt-3 grid grid-cols-2 sm:grid-cols-4 gap-2 sm:gap-4`}>
+        <div
+          className={`bg-green-200  flex flex-col items-center p-3  sm:p-3 rounded cursor-pointer`}
+        >
+          <Image
+            src="/quran-icons.png"
+            alt="Vercel Logo"
+            width={30}
+            height={30}
+            priority
+          />
+          <p className={`text-sm`}>Al-Qur'an</p>
+        </div>
+        <div
+          className={`bg-green-200  flex flex-col items-center p-3  sm:p-3 rounded cursor-pointer`}
+        >
+          <Image
+            src="/quran-icons.png"
+            alt="Vercel Logo"
+            width={30}
+            height={30}
+            priority
+          />
+          <p className={`text-sm`}>Al-Qur'an</p>
+        </div>
+        <div
+          className={`bg-green-200  flex flex-col items-center p-3  sm:p-3 rounded cursor-pointer`}
+        >
+          <Image
+            src="/quran-icons.png"
+            alt="Vercel Logo"
+            width={30}
+            height={30}
+            priority
+          />
+          <p className={`text-sm`}>Al-Qur'an</p>
+        </div>
+        <div
+          className={`bg-green-200  flex flex-col items-center p-3  sm:p-3 rounded cursor-pointer`}
+        >
+          <Image
+            src="/quran-icons.png"
+            alt="Vercel Logo"
+            width={30}
+            height={30}
+            priority
+          />
+          <p className={`text-sm`}>Al-Qur'an</p>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Menu;
