37085f6866cf16cb6c98083c237f1b64f71e5ec5
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-08-25 21:07:56 +0700

    create schedule component

---
+++ src/app/components/schedule.component.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+
+const Schedule = () => {
+  return (
+    <div className={`sm:mt-9 mt-7`}>
+      <div>
+        <h1 className={`font-bold  leading-4`}>
+          Pilih Waktu Sholat Daerah Anda Kab. Tangerang
+        </h1>
+        <p className={`text-sm  `}>Jumat, 25 Agustus / 08 Ṣafar 1445 AH</p>
+      </div>
+      <div className={`mt-3  grid grid-cols-4 gap-2`}>
+        <div
+          className={`bg-red-500 py-2 flex justify-center items-center flex-col rounded`}
+        >
+          <p className={`leading-5  text-sm`}>Subuh</p>
+          <p className={`leading-5 font-semibold  text-sm`}>04:00</p>
+        </div>
+        <div
+          className={`bg-red-500 py-2 flex justify-center items-center flex-col rounded`}
+        >
+          <p className={`leading-5  text-sm`}>Subuh</p>
+          <p className={`leading-5 font-semibold  text-sm`}>04:00</p>
+        </div>
+        <div
+          className={`bg-red-500 py-2 flex justify-center items-center flex-col rounded`}
+        >
+          <p className={`leading-5  text-sm`}>Subuh</p>
+          <p className={`leading-5 font-semibold  text-sm`}>04:00</p>
+        </div>
+        <div
+          className={`bg-red-500 py-2 flex justify-center items-center flex-col rounded`}
+        >
+          <p className={`leading-5  text-sm`}>Subuh</p>
+          <p className={`leading-5 font-semibold  text-sm`}>04:00</p>
+        </div>
+        <div
+          className={`bg-red-500 py-2 flex justify-center items-center flex-col rounded`}
+        >
+          <p className={`leading-5  text-sm`}>Subuh</p>
+          <p className={`leading-5 font-semibold  text-sm`}>04:00</p>
+        </div>
+        <div
+          className={`bg-red-500 py-2 flex justify-center items-center flex-col rounded`}
+        >
+          <p className={`leading-5  text-sm`}>Subuh</p>
+          <p className={`leading-5 font-semibold  text-sm`}>04:00</p>
+        </div>
+        <div
+          className={`bg-red-500 py-2 flex justify-center items-center flex-col rounded`}
+        >
+          <p className={`leading-5  text-sm`}>Subuh</p>
+          <p className={`leading-5 font-semibold  text-sm`}>04:00</p>
+        </div>
+        <div
+          className={`bg-red-500 py-2 flex justify-center items-center flex-col rounded`}
+        >
+          <p className={`leading-5  text-sm`}>Subuh</p>
+          <p className={`leading-5 font-semibold  text-sm`}>04:00</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Schedule;
