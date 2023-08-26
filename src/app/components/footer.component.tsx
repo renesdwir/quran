f74e48ca0f5005e7bc1b50aa691fa04111628413
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-08-26 22:20:18 +0700

    create footer

---
+++ src/app/components/footer.component.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className={`text-center bg-red-200 `}>
+      <small>&copy; Copyright 2023 renesdwir. All Rights Reserved</small>
+    </div>
+  );
+};
+
+export default Footer;
