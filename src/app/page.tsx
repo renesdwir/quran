aacd9b12fcf011c5745784c5985c71f9fef83211
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-08-26 22:23:49 +0700

    add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,20 @@ import { useTheme } from "next-themes";
 import Header from "./components/header.component";
 import Menu from "./components/menu.component";
 import Schedule from "./components/schedule.component";
+import Footer from "./components/footer.component";
 
 export default function Home() {
   const { theme, setTheme } = useTheme();
   return (
-    <main className={`px-5 pt-3 sm:px-9 sm:pt-5`}>
-      <Header />
-      <Menu />
-      <Schedule />
+    <main
+      className={`px-5 pt-3 sm:px-9 sm:pt-5 min-h-screen flex flex-col justify-between`}
+    >
+      <div>
+        <Header />
+        <Menu />
+        <Schedule />
+      </div>
+      <Footer />
     </main>
   );
 }
