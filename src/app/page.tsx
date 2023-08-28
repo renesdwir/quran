e2355fbd8788b23be469790e9a76d6b4bb8d8df6
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-08-28 22:03:19 +0700

    add swith theme button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,6 @@ import Footer from "./components/footer.component";
 export default function Home() {
   const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
-  console.log(theme, "<<");
   useEffect(() => {
     setMounted(true);
   }, []);
@@ -27,7 +26,7 @@ export default function Home() {
         className={`w-fit absolute right-5 top-2 p-2 rounded-md hover:scale-110 active:scale-100 duration-200 bg-slate-200 dark:bg-[#212933]`}
         onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
       >
-        {theme === "dark" ? "light" : "dark"}
+        {theme === "dark" ? "Light" : "Dark"}
       </button>
       <div>
         <Header />
