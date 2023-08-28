67ff1213b593952e93199cc4948fa5403e32a511
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-08-28 22:01:47 +0700

    add setmounted for next-themes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import Image from "next/image";
 import { useState, useEffect } from "react";
 import { useTheme } from "next-themes";
@@ -8,11 +9,26 @@ import Schedule from "./components/schedule.component";
 import Footer from "./components/footer.component";
 
 export default function Home() {
+  const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
+  console.log(theme, "<<");
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
   return (
     <main
       className={`px-5 pt-3 sm:px-9 sm:pt-5 min-h-screen flex flex-col justify-between`}
     >
+      <button
+        className={`w-fit absolute right-5 top-2 p-2 rounded-md hover:scale-110 active:scale-100 duration-200 bg-slate-200 dark:bg-[#212933]`}
+        onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      >
+        {theme === "dark" ? "light" : "dark"}
+      </button>
       <div>
         <Header />
         <Menu />
