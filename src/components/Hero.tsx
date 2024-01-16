675c92ede937cd90ce4beb7a3a8cec7b19a6ecda
Renes Dwi Riwanto <renesriwanto@gmail.com>
2024-01-16 19:50:40 +0700

    init hero

---
+++ src/components/Hero.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import Logo from "./icons/logo";
+import FakeSearch from "./search/FakeSearch";
+import { Button } from "@/components/ui/button";
+
+export default function Hero() {
+  return (
+    <>
+      <div className="flex flex-col items-center  py-8 gap-y-6 relative overflow-hidden">
+        <Logo className="h-[1500px] w-[1500px] object-cover dark:text-zinc-300 text-zinc-300 dark:opacity-[0.06] opacity-[0.3] -z-10 fill-current absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2" />
+        <Logo className="h-40 w-40 dark:text-white text-black fill-current" />
+        <div className="w-full max-w-2xl px-4">
+          <FakeSearch />
+        </div>
+        <div className="flex flex-row flex-wrap justify-center gap-2 px-4">
+          <Button variant="outline">About The Quran</Button>
+          <Button variant="outline">Al Mulk</Button>
+          <Button variant="outline">Yaseen</Button>
+          <Button variant="outline">Al Kahf</Button>
+          <Button variant="outline">Al Waqi'ah</Button>
+        </div>
+      </div>
+    </>
+  );
+}
