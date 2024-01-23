f9bf9a91c15dffe022d6925f67f7019d3fda006c
Renes Dwi Riwanto <renesriwanto@gmail.com>
2024-01-23 21:25:43 +0700

    passing props

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,16 +1,15 @@
-import Image from "next/image";
 import Logo from "./icons/logo";
-import FakeSearch from "./search/FakeSearch";
 import { Button } from "@/components/ui/button";
+import Search from "./search/Search";
 
-export default function Hero() {
+export default function Hero({ search }: { search: string }) {
   return (
     <>
       <div className="flex flex-col items-center  py-8 gap-y-6 relative overflow-hidden">
         <Logo className="h-[1500px] w-[1500px] object-cover dark:text-zinc-300 text-zinc-300 dark:opacity-[0.06] opacity-[0.3] -z-10 fill-current absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2" />
         <Logo className="h-40 w-40 dark:text-white text-black fill-current" />
         <div className="w-full max-w-2xl px-4">
-          <FakeSearch />
+          <Search search={search} />
         </div>
         <div className="flex flex-row flex-wrap justify-center gap-2 px-4">
           <Button variant="outline">About The Quran</Button>
