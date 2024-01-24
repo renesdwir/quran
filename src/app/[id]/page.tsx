b0a0248ce84408137fd7026384e920402ab7169c
Renes Dwi Riwanto <renesriwanto@gmail.com>
2024-01-24 22:33:48 +0700

    try ssg

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,7 +1,24 @@
-export default function Page() {
+import { DataSurah, Surah } from "@/models/surah";
+
+export async function generateStaticParams() {
+  const res = await fetch("https://api.quran.gading.dev/surah");
+  const surah: DataSurah = await res.json();
+  return surah.data.map((data) => ({
+    id: data.number.toString(),
+  }));
+}
+async function getSurah(id: string) {
+  const res = await fetch(`https://api.quran.gading.dev/surah/${id}`);
+  const surah = await res.json();
+  return surah.data;
+}
+
+export default async function Page({ params }: { params: { id: string } }) {
+  const { id } = params;
+  const surah: Surah = await getSurah(id);
   return (
     <main className="">
-      <h1>id</h1>
+      <h1>{surah.name.transliteration.id}</h1>
     </main>
   );
 }
