3962687a6c07481c5120a7f0c6aa61e36a824d8a
Renes Dwi Riwanto <renesriwanto@gmail.com>
2024-01-29 22:21:19 +0700

    add tooltip

--- src/components/ayah/Ayah.tsx
+++ src/components/ayah/Ayah.tsx
@@ -1,20 +1,38 @@
 import { Ayat } from "@/models/surah";
+import { Copy, FileText, PlayCircle, Share2 } from "lucide-react";
+import WithTooltip from "../Tooltip";
 
 export default function Ayah({ ayah }: { ayah: Ayat }) {
   return (
-    <div className="flex flex-col gap-4 border-b pb-7 md:pb-14">
+    <div className="flex flex-col gap-4 border-b pb-7 md:pb-10">
       <div className="flex justify-between md:gap-4">
-        <div className="w-8 h-8 md:w-11 md:h-11 mr-4 bg-zinc-300 my-5  dark:bg-opacity-[0.1] bg-opacity-[0.6] rotate-45 flex justify-center items-center rounded-md">
-          <p className="text-xs md:text-base -rotate-45 font-semibold group-hover:text-background">
-            {ayah.nomorAyat}
-          </p>
+        <div className="w-8 h-8 md:w-11 md:h-11 mr-4 bg-muted my-5 rotate-45 flex justify-center items-center rounded-md">
+          <p className="text-xs md:text-base -rotate-45 ">{ayah.nomorAyat}</p>
         </div>
-        <div className="font-arabic text-2xl md:text-3xl text-right flex-1  dark:bg-opacity-[0.1] bg-opacity-[0.6] p-4 rounded-md leading-loose md:leading-loose">
+        <div className="font-arabic text-2xl md:text-3xl text-right flex-1  dark:bg-opacity-[0.1] bg-opacity-[0.6]  py-1 rounded-md  leading-loose md:leading-[70px]">
           {ayah.teksArab}
         </div>
       </div>
       <p className="text-muted-foreground text-sm md:text-base">{ayah.teksLatin}</p>
       <p className="text-sm md:text-base">{ayah.teksIndonesia}</p>
+      <div className="flex justify-between mt-4">
+        <div className="flex gap-5">
+          <WithTooltip text="Play Audio">
+            <PlayCircle className="" strokeWidth={1.5} />
+          </WithTooltip>
+          <WithTooltip text="Copy">
+            <Copy className=" " strokeWidth={1.5} />
+          </WithTooltip>
+          <WithTooltip text="Tafsir">
+            <FileText className=" " strokeWidth={1.5} />
+          </WithTooltip>
+        </div>
+        <div>
+          <WithTooltip text="Bagikan">
+            <Share2 className=" " strokeWidth={1.5} />
+          </WithTooltip>
+        </div>
+      </div>
     </div>
   );
 }
