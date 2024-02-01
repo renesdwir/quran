f206b053aabc3429755ebeecaf8a2797a885c4df
Renes Dwi Riwanto <renesriwanto@gmail.com>
2024-02-01 21:27:35 +0700

    logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,18 @@
 import Image from "next/image";
 import { ToggleThemeButton } from "./buttons/ToggleThemeButton";
 import Logo from "./icons/logo";
+import Link from "next/link";
 
 export default function Navbar() {
   return (
     <>
       <div className="flex justify-between items-center p-4 border-b ">
-        <div className="flex items-center  gap-3">
+        <Link href={"/"} className="flex items-center  gap-3">
           <h1 className="text-xl font-bold">Al-Qur'an Web</h1>
-          <div className=" rounded-full">
-            <Logo className="w-10 h-10 dark:text-white text-black fill-current" />
+          <div className=" rounded-full ">
+            <Logo className="w-10 h-10 fill-foreground" />
           </div>
-        </div>
+        </Link>
         <ToggleThemeButton />
       </div>
     </>
