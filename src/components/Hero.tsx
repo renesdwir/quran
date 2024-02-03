d05459d2f34f62ac8a69dccf622f5a37ab949447
Renes Dwi Riwanto <renesriwanto@gmail.com>
2024-02-03 22:02:34 +0700

    rename logo

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,4 @@
-import Logo from "./icons/logo";
+import Logo from "./icons/LogoSVG";
 import { Button } from "@/components/ui/button";
 import Search from "./search/Search";
 import Link from "next/link";
