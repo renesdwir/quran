43e2966c62a03fb2282b2dd7081c86fed48bc95c
Renes Dwi Riwanto <renesriwanto@gmail.com>
2024-02-04 19:21:16 +0700

    change logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,11 @@
-import Image from "next/image";
 import { ToggleThemeButton } from "./buttons/ToggleThemeButton";
-import Logo from "./icons/logo";
-import Link from "next/link";
+import Logo from "./Logo";
 
 export default function Navbar() {
   return (
     <>
       <div className="flex justify-between items-center p-4 border-b ">
-        <Link href={"/"} className="flex items-center  gap-3">
-          <h1 className="text-xl font-bold">Al-Qur'an Web</h1>
-          <div className=" rounded-full ">
-            <Logo className="w-10 h-10 fill-foreground" />
-          </div>
-        </Link>
+        <Logo />
         <ToggleThemeButton />
       </div>
     </>
