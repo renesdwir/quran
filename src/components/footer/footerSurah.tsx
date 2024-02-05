4f85b2627f73fe59f518b92fc8fd84649c3be37f
Renes Dwi Riwanto <renesriwanto@gmail.com>
2024-02-05 21:15:32 +0700

    fix link button

--- src/components/footer/footerSurah.tsx
+++ src/components/footer/footerSurah.tsx
@@ -1,6 +1,7 @@
 import { MoveLeft, MoveRight } from "lucide-react";
 import { Button } from "../ui/button";
 import { NextPrevSurah } from "@/models/surah";
+import Link from "next/link";
 
 interface FooterSurahProps {
   suratSebelumnya: NextPrevSurah;
@@ -15,25 +16,25 @@ export default function FooterSurah({
   noSurah,
 }: FooterSurahProps) {
   return (
-    <div className="grid grid-cols-3 mt-5 items-center">
+    <div className="grid grid-cols-3 items-center py-10">
       {suratSebelumnya ? (
-        <div className="">
+        <Link href={`/${suratSebelumnya.nomor}`}>
           <Button variant={"secondary"} className="flex items-center gap-3">
             <MoveLeft /> <p>{suratSebelumnya.namaLatin}</p>
           </Button>
-        </div>
+        </Link>
       ) : (
         <div></div>
       )}
-      <div className=" text-center text-xs">
+      <div className="text-center text-xs">
         Surat ke-{noSurah} {nama}
       </div>
       {suratSelanjutnya ? (
-        <div className="flex justify-end">
+        <Link href={`/${suratSelanjutnya.nomor}`} className="flex justify-end">
           <Button variant={"secondary"} className="flex items-center gap-3">
             <p>{suratSelanjutnya.namaLatin}</p> <MoveRight />
           </Button>
-        </div>
+        </Link>
       ) : (
         <div></div>
       )}
