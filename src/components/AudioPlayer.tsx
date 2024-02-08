975bb1f8cc99a20f6873612a07210d956bfde652
Renes Dwi Riwanto <renesriwanto@gmail.com>
2024-02-08 23:32:35 +0700

    audioplayer button

--- src/components/AudioPlayer.tsx
+++ src/components/AudioPlayer.tsx
@@ -1,3 +1,28 @@
-export default function AudioPlayer() {
-  return <p>audio player</p>;
+"use client";
+import { PlayCircle, PauseCircle } from "lucide-react";
+import WithTooltip from "./Tooltip";
+import { useState } from "react";
+
+export default function AudioPlayer({
+  play,
+  handleAudio,
+  id,
+}: {
+  play: boolean;
+  id: number;
+  handleAudio: (id: number) => void;
+}) {
+  return (
+    <>
+      {play ? (
+        <WithTooltip text="Pause Audio">
+          <PauseCircle strokeWidth={1.5} onClick={() => handleAudio(id)} />
+        </WithTooltip>
+      ) : (
+        <WithTooltip text="Play Audio">
+          <PlayCircle strokeWidth={1.5} onClick={() => handleAudio(id)} />
+        </WithTooltip>
+      )}
+    </>
+  );
 }
