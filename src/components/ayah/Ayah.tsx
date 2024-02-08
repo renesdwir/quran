10fc647d7007daf44be0f0e8b64c3c46d72072fa
Renes Dwi Riwanto <renesriwanto@gmail.com>
2024-02-08 22:16:35 +0700

    add audioplayer

--- src/components/ayah/Ayah.tsx
+++ src/components/ayah/Ayah.tsx
@@ -1,7 +1,8 @@
 import { Ayat } from "@/models/surah";
-import { Copy, FileText, PlayCircle, Share2 } from "lucide-react";
+import { Copy, FileText, Share2 } from "lucide-react";
 import WithTooltip from "../Tooltip";
 import AyahFrame from "../icons/ayahFrame";
+import AudioPlayer from "../AudioPlayer";
 
 export default function Ayah({ ayah }: { ayah: Ayat }) {
   return (
@@ -22,16 +23,14 @@ export default function Ayah({ ayah }: { ayah: Ayat }) {
       <p className="text-muted-foreground text-sm md:text-base">{ayah.teksLatin}</p>
       <p className="text-sm md:text-base">{ayah.teksIndonesia}</p>
       <div className="flex justify-between mt-4 items-center">
-        <div className="flex gap-5 ">
+        <div className="flex gap-5 items-center">
+          <AudioPlayer />
           <audio controls>
             <source
               src="https://equran.nos.wjv-1.neo.id/audio-partial/Misyari-Rasyid-Al-Afasi/001001.mp3"
               type="audio/mpeg"
             />
           </audio>
-          <WithTooltip text="Play Audio">
-            <PlayCircle className="" strokeWidth={1.5} />
-          </WithTooltip>
           <WithTooltip text="Copy">
             <Copy className=" " strokeWidth={1.5} />
           </WithTooltip>
