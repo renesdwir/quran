a61caea61c63557dccf5b40980fd326c6fb7d518
Renes Dwi Riwanto <renesriwanto@gmail.com>
2024-02-12 21:38:31 +0700

    fix element

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,18 +1,11 @@
-import Ayah from "@/components/ayah/Ayah";
 import AyahList from "@/components/ayah/AyahList";
 import FooterSurah from "@/components/footer/footerSurah";
-import { Button } from "@/components/ui/button";
 import { DataDetailSurah, ResponseListSurah } from "@/models/surah";
-import { MoveRight, MoveLeft, ChevronRight } from "lucide-react";
 
 export const dynamicParams = false;
 export async function generateStaticParams() {
   const res = await fetch("https://equran.id/api/v2/surat");
   const surah: ResponseListSurah = await res.json();
-  let temp = surah.data.map((data) => ({
-    id: data.nomor.toString(),
-  }));
-  // console.log(temp);
   let getId = surah.data.map((data) => ({
     id: data.nomor.toString(),
   }));
